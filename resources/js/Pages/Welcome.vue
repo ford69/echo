e37378a4d5ee869ed105667fc14ed1c79d824abd
Clifford Manu <clifford@myzeepay.com>
2024-06-06 12:24:49 +0000

    front end design for authentication

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -35,7 +35,7 @@ const toggleMenu = () => {
 
 
 <template>
-    <div class="w-full min-h-screen bg-gradient-to-tr from-cyan-500 to-red-600">
+    <div class="w-full min-h-screen bg-white">
         <header class="bg-[#FCF8F1] bg-opacity-30">
             <div class="px-4 mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between h-16 lg:h-20">
@@ -59,7 +59,7 @@ const toggleMenu = () => {
                         </svg>
                     </button>
 
-                    <div v-if="menuOpen" class="lg:flex lg:items-center lg:justify-center lg:space-x-10">
+                    <!-- <div v-if="menuOpen" class="lg:flex lg:items-center lg:justify-center lg:space-x-10">
                         <a href="#" title=""
                             class="text-base text-black transition-all duration-200 hover:text-opacity-80">Features</a>
                         <a href="#" title=""
@@ -68,12 +68,12 @@ const toggleMenu = () => {
                             class="text-base text-black transition-all duration-200 hover:text-opacity-80">Resources</a>
                         <a href="#" title=""
                             class="text-base text-black transition-all duration-200 hover:text-opacity-80">Pricing</a>
-                    </div>
+                    </div> -->
 
-                    <a href="#" title=""
+                    <a :href="route('register')"  title=""
                         class="hidden lg:inline-flex items-center justify-center px-5 py-2.5 text-base transition-all duration-200 hover:bg-yellow-300 hover:text-black focus:text-black focus:bg-yellow-300 font-semibold text-white bg-black rounded-full"
                         role="button">
-                        Join Now
+                        Sign up
                     </a>
                 </div>
             </div>
@@ -91,7 +91,7 @@ const toggleMenu = () => {
                         <a :href="route('register')" title=""
                             class="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400"
                             role="button">
-                            Join for free
+                            Sign up
                             <svg class="w-6 h-6 ml-8 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
